52bde876d5d152c854cac1988fad461935919e07
luqman hardianto <luqmanhardianto@gmail.com>
2026-08-29 09:44:39 +0700

    create empty function display_result

---
+++ cli.py
@@ -0,0 +1,23 @@
+def ask_choice(question,options):
+    print(f"\n{question}")
+
+    for index, label in options.items():
+        print(f"{index} - {label}")
+
+    while True:
+        try:
+            choice = input("> ")
+            return options[choice]
+        except (ValueError, KeyError):
+            print("invalid choice. try again.")
+
+def ask_number(question, unit=""):
+    while True:
+        try:
+            value = float(input(f"{question} {unit}: "))
+            return value
+        except ValueError:
+            print("please enter a valid number.")
+
+def display_result():
+    return
