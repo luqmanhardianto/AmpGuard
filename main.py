880f046c5d3c2d58eb21a67a2ab6f8330f95f599
luqman hardianto <luqmanhardianto@gmail.com>
2026-08-29 08:59:03 +0700

    create reusable function ask_choice for question/menu cli

--- main.py
+++ main.py
@@ -1,52 +1,33 @@
-import math
+import calculator
+import menus
 
 power_factor = 0.75
 
 readme = f"""
     AmpGuard is circuit breaker size calculator 
-    based on {list(method_type.values())} method
-    for now this app can calculated for a few limitation
-    load - {list(load_type.values())}
-    voltage - {list(voltage_type.values())} VAC
-    safety factor - {safety_factor} % dependent on load type
-    power factor - {power_factor}"""
+    """
 
-def calculate_load(load, voltage,power_factor, safety_factor):
-    """
-    calculate ampere using load method
-    """
-    return (load/(math.sqrt(3)*voltage*power_factor))*(1+(safety_factor/100))
+def ask_choice(question,options):
+    print(f"\n{question}")
 
-def calculate_current(current,safety_factor):
-    """
-    calculate ampere using current method
-    """
-    return (current*(1+(safety_factor/100)))
+    for index, label in options.items():
+        print(f"{index} - {label}")
+
+    while True:
+        try:
+            choice = input("> ")
+            return options[choice]
+        except (ValueError, KeyError):
+            print("invalid choice. try again.")
+
 
 while True:
     print(readme)
-
-    method_type_list = ""
-    for list in method_type:
-        method_type_list += f"\n{list} - {method_type[list]}"
-    print(method_type_list)
-    method = input("choose base calculator method :")
-
-    load_type_list = ""
-    for list in load_type:
-        load_type_list += f"\n{list} - {load_type[list]}"
-    print(load_type_list)
-    load = input("choose load type :")
-
-    load_input = int(input(f"What is the {method_type[method]} of your {load_type[load]} in ({unit_type[method_type[method]]}) :"))
-
-    result = calculate_load(
-        load=load_input, 
-        voltage=voltage_type["2"], 
-        power_factor=power_factor,
-        safety_factor=safety_factor["motor"]
+    method = ask_choice(
+        question="choose base calculator method:",
+        options=menus.CALCULATOR_METHOD
         )
     
-    print(f"result ampere for your load is : {result} A")
+    print(f"result ampere for your load is : {method} A")
     break
 
