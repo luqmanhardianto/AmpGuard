5f8439dcbd505279958a7ed6da30395aa6359681
luqman hardianto <luqmanhardianto@gmail.com>
2026-08-31 08:55:43 +0700

    calculate ampere rating using current method

--- calculator.py
+++ calculator.py
@@ -1,2 +1,12 @@
+from menus import SAFETY_FACTOR
+from cli import display_result
+
 def calculate_current(collect_input):
-    return 
+    margin_of_safety = SAFETY_FACTOR[collect_input["load_type"]]
+    current = collect_input["current"]
+    result = current (1*(margin_of_safety/100))
+    return display_result(
+        answer="circuit breaker size is:",
+        result=result,
+        unit="A"
+    )
