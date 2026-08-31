05daf264c4cbb0266d6ee6e5cce0e027ea3e928f
luqman hardianto <luqmanhardianto@gmail.com>
2026-08-31 14:16:42 +0700

    change calculate_load for power_factor

--- calculator.py
+++ calculator.py
@@ -33,11 +33,11 @@ def calculate_load(collect_input):
 
     # 220v calculation
     if voltage_type == "single_phase":
-        result = (power/(VOLTAGE[voltage_type]*(power_factor/100)))*(1+(margin_of_safety/100))
+        result = (power/(VOLTAGE[voltage_type]*power_factor))*(1+(margin_of_safety/100))
 
     # 380v calculation
     elif voltage_type == "three_phase":
-        result = (power/(sqrt(3)*VOLTAGE[voltage_type]*(power_factor/100)))*(1+(margin_of_safety/100))
+        result = (power/(sqrt(3)*VOLTAGE[voltage_type]*power_factor))*(1+(margin_of_safety/100))
     return display_result(
         answer="circuit breaker size is:",
         result=result,
