ef00710c730ff297108882cf58ec2caba762551c
luqman hardianto <luqmanhardianto@gmail.com>
2026-08-31 09:33:45 +0700

    implement func calculate_load

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from cli import ask_choice
 from menus import CALCULATOR_METHOD
 from inputs import collect_current_inputs, collect_load_inputs
-from calculator import calculate_current
+from calculator import calculate_current, calculate_load
 
 INPUT_COLLECTORS = {
     "current":collect_current_inputs,
@@ -10,6 +10,7 @@ INPUT_COLLECTORS = {
 
 CALCULATE = {
     "current":calculate_current,
+    "load":calculate_load
 }
 
 power_factor = 0.75
