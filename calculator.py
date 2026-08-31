ca55f041cf1bb2efd4108707d6cf21329f43d386
luqman hardianto <luqmanhardianto@gmail.com>
2026-08-31 09:29:13 +0700

    calculate for different voltage 380v and 220v

--- calculator.py
+++ calculator.py
@@ -1,9 +1,10 @@
-from menus import SAFETY_FACTOR
+from menus import SAFETY_FACTOR, VOLTAGE_TYPE
 from cli import display_result
 
 def calculate_current(collect_input):
-    margin_of_safety = SAFETY_FACTOR[collect_input['load_type']]
-    current = collect_input['current']
+    load_type =collect_input["load_type"]
+    margin_of_safety = SAFETY_FACTOR[load_type]
+    current = collect_input["current"]
     result = current * (1+(margin_of_safety/100))
     return  display_result(
         answer="circuit breaker size is:",
@@ -12,7 +13,22 @@ def calculate_current(collect_input):
         )
 
 def calculate_load(collect_input):
-    load_type = collect_input["load-type"]
-    voltage_type = collect_input{"voltage_type"}
+    load_type = collect_input["load_type"]
+    voltage_type = collect_input["voltage_type"]
     power = collect_input["power"]
-    return print(f"{load_type} {voltage_type} {power}")
+    margin_of_safety = SAFETY_FACTOR[load_type]
+    if voltage_type == VOLTAGE_TYPE["1"]:
+        result = (power/voltage_type)*(1+(margin_of_safety/100))
+    elif voltage_type == VOLTAGE_TYPE["2"]:
+        result = (power/voltage_type)*(1+(margin_of_safety/100))
+    return display_result(
+        answer="circuit breaker size is:",
+        result=result,
+        unit="A"
+        )
+
+calculate_load({
+    "load_type":"motor",
+    "voltage_type":380,
+    "power":2200
+    })
