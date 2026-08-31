1e76ecb16fd98a0f231fb37f54da91abfa45fb02
luqman hardianto <luqmanhardianto@gmail.com>
2026-08-31 09:40:51 +0700

    test func calculate_load

--- calculator.py
+++ calculator.py
@@ -1,5 +1,6 @@
 from menus import SAFETY_FACTOR, VOLTAGE_TYPE
 from cli import display_result
+from math import sqrt
 
 def calculate_current(collect_input):
     load_type =collect_input["load_type"]
@@ -16,11 +17,16 @@ def calculate_load(collect_input):
     load_type = collect_input["load_type"]
     voltage_type = collect_input["voltage_type"]
     power = collect_input["power"]
+    power_factor = collect_input["power_factor"]
     margin_of_safety = SAFETY_FACTOR[load_type]
+
+    # 220v calculation
     if voltage_type == VOLTAGE_TYPE["1"]:
-        result = (power/voltage_type)*(1+(margin_of_safety/100))
+        result = (power/(voltage_type*(power_factor/100)))*(1+(margin_of_safety/100))
+
+    # 380v calculation
     elif voltage_type == VOLTAGE_TYPE["2"]:
-        result = (power/voltage_type)*(1+(margin_of_safety/100))
+        result = (power/(sqrt(3)*voltage_type*(power_factor/100)))*(1+(margin_of_safety/100))
     return display_result(
         answer="circuit breaker size is:",
         result=result,
@@ -30,5 +36,6 @@ def calculate_load(collect_input):
 calculate_load({
     "load_type":"motor",
     "voltage_type":380,
-    "power":2200
+    "power":2200,
+    "power_factor":100
     })
